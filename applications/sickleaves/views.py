c6468888dbdecd3f4b82291fe7f117a026fa1d06
treflina <treflina@wp.pl>
2021-12-18 19:44:01 +0100

    mails content

--- applications/sickleaves/views.py
+++ applications/sickleaves/views.py
@@ -19,18 +19,17 @@ class SickleavesListView(TopManagerPermisoMixin, ListView):
     context_object_name = 'sickleaves'
     template_name = "sickleaves/sickleaves.html"
     login_url = reverse_lazy('users_app:user-login')
-    
+
     def get_queryset(self):
         return Sickleave.objects.all().order_by('-issue_date')
+
 
 class SickleaveCreateView(TopManagerPermisoMixin, CreateView):
     template_name = "sickleaves/add_sickleave.html"
     model = Sickleave
-    form_class = SickleaveForm 
+    form_class = SickleaveForm
     success_url = reverse_lazy('sickleaves_app:sickleaves')
     login_url = reverse_lazy('users_app:user-login')
-
-
 
     def form_valid(self, form):
         employee = form.cleaned_data["employee"]
@@ -38,24 +37,26 @@ class SickleaveCreateView(TopManagerPermisoMixin, CreateView):
         end_date = form.cleaned_data["end_date"]
         type = form.cleaned_data["type"]
 
-        subject = f"chorobowe {employee.first_name} {employee.last_name} ({start_date} - {end_date})" 
+        subject = f"chorobowe {employee.first_name} {employee.last_name} ({start_date} - {end_date})"
         message = f"Dzień dobry,\r\n {employee.first_name} {employee.last_name} przebywa na zwolnieniu lekarskim ({type}) w dniach {start_date} - {end_date}.\r\n \r\nWiadomość wygenerowana automatycznie."
-        send= User.objects.filter(Q(role='S')|Q(id = employee.manager.id))
-        print(send)
-        send_to_people = User.objects.filter(Q(role='S')|Q(id = employee.manager.id)).distinct()
-        print(send_to_people)
+        send = User.objects.filter(Q(role='S') | Q(id=employee.manager.id))
+        # print(send)
+        send_to_people = User.objects.filter(
+            Q(role='S') | Q(id=employee.manager.id)).distinct()
+        # print(send_to_people)
         send_to_people_list = [person.work_email for person in send_to_people]
-        print(send_to_people_list)
-        
+        # print(send_to_people_list)
+
         send_mail(
             subject,
             message,
             settings.EMAIL_HOST_USER,
             send_to_people_list,
             fail_silently=False,
-        ) 
+        )
 
         return super(SickleaveCreateView, self).form_valid(form)
+
 
 class SickleaveUpdateView(TopManagerPermisoMixin, UpdateView):
     model = Sickleave
@@ -64,8 +65,7 @@ class SickleaveUpdateView(TopManagerPermisoMixin, UpdateView):
     success_url = reverse_lazy('sickleaves_app:sickleaves')
     login_url = reverse_lazy('users_app:user-login')
 
-def delete_sickleave(request,pk):
+
+def delete_sickleave(request, pk):
     sickleave_to_delete = Sickleave.objects.get(id=pk).delete()
     return HttpResponseRedirect(reverse('sickleaves_app:sickleaves'))
-
-
--- pdf_creator.py
+++ pdf_creator.py
@@ -8,44 +8,46 @@ from reportlab.pdfgen import canvas
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 
 
-
 stylesheet = getSampleStyleSheet()
 pdfmetrics.registerFont(TTFont('TNR', './static/fonts/signikan.ttf'))
 
 
 def create_pdf(data, fileName, title, start_date, end_date, person, position):
 
-      pdf=SimpleDocTemplate(
-            fileName,
-            pagesize=landscape(A4),
-            rightMargin=100,
-            leftMargin=100,
-            topMargin=50,
-            bottomMargin=50
-      )
+    pdf = SimpleDocTemplate(
+        fileName,
+        pagesize=landscape(A4),
+        rightMargin=100,
+        leftMargin=100,
+        topMargin=50,
+        bottomMargin=50
+    )
 
-      table=Table(data)
-      elems=[]
-      style = ParagraphStyle(
-            name='Title',
-            fontName='TNR',
-            fontSize=12,
-            alignment=TA_CENTER,)
-      style1 = ParagraphStyle(
-            name='footer',
-            fontName='TNR',
-            fontSize=10,
-            alignment=TA_CENTER,)
-      elems.append(Paragraph(f"{title} w okresie od {start_date} do {end_date}", style=style))
-      elems.append(Spacer(1, 16))
-      if position != "":
-        elems.append(Paragraph(f"{person} (stanowisko: {position})" , style=style))
+    table = Table(data)
+    elems = []
+    style = ParagraphStyle(
+        name='Title',
+        fontName='TNR',
+        fontSize=12,
+        alignment=TA_CENTER,)
+    style1 = ParagraphStyle(
+        name='footer',
+        fontName='TNR',
+        fontSize=10,
+        alignment=TA_CENTER,)
+    elems.append(
+        Paragraph(f"{title} w okresie od {start_date} do {end_date}", style=style))
+    elems.append(Spacer(1, 16))
+    if position != "":
+        elems.append(
+            Paragraph(f"{person} (stanowisko: {position})", style=style))
         elems.append(Spacer(1, 20))
-      elems.append(table)
-      style_table=TableStyle([('FONTNAME', (0, 0), (-1, -1), 'TNR'), ('FONTSIZE', (0, 0), (-1, -1), 12),('GRID',(0,0),(-1,-1),1,colors.black)])
-      table.setStyle(style_table)
-      elems.append(Spacer(1, 20))
-      elems.append(Paragraph("Wygenerowano automatycznie z Wnioski urlopowe 1.0", style=style1))
+    elems.append(table)
+    style_table = TableStyle([('FONTNAME', (0, 0), (-1, -1), 'TNR'), ('FONTSIZE',
+                             (0, 0), (-1, -1), 12), ('GRID', (0, 0), (-1, -1), 1, colors.black)])
+    table.setStyle(style_table)
+    elems.append(Spacer(1, 20))
+    elems.append(
+        Paragraph("Wygenerowano automatycznie z Moja MBP", style=style1))
 
-      pdf.build(elems)
-
+    pdf.build(elems)
