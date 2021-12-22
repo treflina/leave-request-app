b8a7e7e1fa45b7d65470363cab0d4c6807dda325
treflina <treflina@wp.pl>
2021-12-22 17:07:19 +0100

    update-pass modal-success-msg menu request.user

--- applications/users/forms.py
+++ applications/users/forms.py
@@ -5,28 +5,28 @@ from django.db.models import Q
 
 from .models import User
 
+
 class UserRegisterForm(forms.ModelForm):
 
     password1 = forms.CharField(
-        label = 'Hasło',
-        required = True,
-        widget = forms.PasswordInput(
-            attrs ={
+        label='Hasło',
+        required=True,
+        widget=forms.PasswordInput(
+            attrs={
                 'placeholder': 'Hasło'
             }
         )
     )
     password2 = forms.CharField(
-        label = 'Powtórz hasło',
-        required = True,
-        widget = forms.PasswordInput(
-            attrs ={
+        label='Powtórz hasło',
+        required=True,
+        widget=forms.PasswordInput(
+            attrs={
                 'placeholder': 'Powtórz hasło'
             }
         )
     )
 
-        
     class Meta:
 
         model = User
@@ -46,47 +46,48 @@ class UserRegisterForm(forms.ModelForm):
             'current_leave',
             'contract_end',
             'additional_info',
+            'is_active',
+            'is_superuser',
         )
         widgets = {
             'contract_end': forms.DateInput(
                 format='%d.%m.%y',
-                attrs = {
+                attrs={
                     'type': 'date',
                 },
-                ),
-    
+            ),
+
         }
-    
+
     def clean_password2(self):
         if self.cleaned_data['password1'] != self.cleaned_data['password2']:
             self.add_error('password2', "Wprowadzone hasła nie są identyczne.")
-    
+
     def __init__(self, *args, **kwargs):
         super(UserRegisterForm, self).__init__(*args, **kwargs)
         self.fields['manager'] = forms.ModelChoiceField(
-           queryset=User.objects.filter(~Q(role="P")).order_by('last_name'), required=False
-    )
-
+            queryset=User.objects.filter(~Q(role="P") & Q(is_active=True)).order_by('last_name'), required=False
+        )
 
 
 class LoginForm(forms.Form):
-    
+
     username = forms.CharField(
-        label = 'Nazwa użytkownika',
-        required = True,
-        widget = forms.TextInput(
-            attrs ={
+        label='Nazwa użytkownika',
+        required=True,
+        widget=forms.TextInput(
+            attrs={
                 'placeholder': 'Nazwa użytkownika',
             }
         )
     )
 
     password = forms.CharField(
-        label = 'Hasło',
-        required = True,
-        widget = forms.PasswordInput(
-            attrs ={
-                'placeholder': 'Hasło', 
+        label='Hasło',
+        required=True,
+        widget=forms.PasswordInput(
+            attrs={
+                'placeholder': 'Hasło',
             }
         )
     )
@@ -97,28 +98,39 @@ class LoginForm(forms.Form):
         password = self.cleaned_data.get('password')
 
         if not authenticate(username=username, password=password):
-            raise forms.ValidationError('Dane podane do logowania nie są prawidłowe.')
+            raise forms.ValidationError(
+                'Dane podane do logowania nie są prawidłowe.')
 
         return cleaned_data
 
+
 class UpdatePasswordForm(forms.Form):
-    
+
     password1 = forms.CharField(
-        label = 'Dotychczasowe hasło',
-        required = True,
-        widget = forms.PasswordInput(
-            attrs ={
+        label='Dotychczasowe hasło',
+        required=True,
+        widget=forms.PasswordInput(
+            attrs={
                 'placeholder': 'Dotychczasowe hasło',
             }
         )
     )
     password2 = forms.CharField(
-        label = 'Nowe hasło',
-        required = True,
-        widget = forms.PasswordInput(
-            attrs ={
+        label='Nowe hasło',
+        required=True,
+        widget=forms.PasswordInput(
+            attrs={
                 'placeholder': 'Nowe hasło',
             }
         )
     )
-  
+
+    password3 = forms.CharField(
+        label='Powtórz nowe hasło',
+        required=True,
+        widget=forms.PasswordInput(
+            attrs={
+                'placeholder': 'Powtórz nowe hasło',
+            }
+        )
+    )
--- applications/users/views.py
+++ applications/users/views.py
@@ -6,8 +6,10 @@ from django.urls import reverse_lazy, reverse
 from django.db.models import Q
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.http import HttpResponseRedirect
+from django import forms
 
 from django.views.generic import (
     View,
@@ -24,7 +26,7 @@ from django.views.generic.edit import (
 from applications.users.mixins import TopManagerPermisoMixin
 
 from .forms import (
-    UserRegisterForm, 
+    UserRegisterForm,
     LoginForm,
     UpdatePasswordForm,
 )
@@ -37,7 +39,6 @@ from applications.sickleaves.models import Sickleave
 
 class UserRegisterView(TopManagerPermisoMixin, FormView):
 
-
     template_name = 'users/register.html'
     form_class = UserRegisterForm
     success_url = reverse_lazy('users_app:user-register')
@@ -48,27 +49,26 @@ class UserRegisterView(TopManagerPermisoMixin, FormView):
         User.objects.create_user(
             form.cleaned_data['username'],
             form.cleaned_data['password1'],
-            first_name = form.cleaned_data['first_name'],
-            last_name = form.cleaned_data['last_name'],
-            position = form.cleaned_data['position'],
-            email = form.cleaned_data['email'],
-            work_email = form.cleaned_data['work_email'],
-            position_addinfo = form.cleaned_data['position_addinfo'],
-            workplace = form.cleaned_data['workplace'],
+            first_name=form.cleaned_data['first_name'],
+            last_name=form.cleaned_data['last_name'],
+            position=form.cleaned_data['position'],
+            role=form.cleaned_data['role'],
+            email=form.cleaned_data['email'],
+            work_email=form.cleaned_data['work_email'],
+            position_addinfo=form.cleaned_data['position_addinfo'],
+            workplace=form.cleaned_data['workplace'],
             manager=form.cleaned_data['manager'],
             working_hours=form.cleaned_data['working_hours'],
-            annual_leave = form.cleaned_data['annual_leave'],
-            current_leave = form.cleaned_data['current_leave'],
+            annual_leave=form.cleaned_data['annual_leave'],
+            current_leave=form.cleaned_data['current_leave'],
             contract_end=form.cleaned_data['contract_end'],
             additional_info=form.cleaned_data['additional_info'],
         )
-    
+
         return super(UserRegisterView, self).form_valid(form)
 
 
-
 class LoginUser(FormView):
-
 
     template_name = 'users/login.html'
     form_class = LoginForm
@@ -86,7 +86,6 @@ class LoginUser(FormView):
 
 class LogoutView(View):
 
-
     def get(self, request, *args, **kargs):
         logout(request)
         return HttpResponseRedirect(
@@ -96,31 +95,34 @@ class LogoutView(View):
         )
 
 
-class UpdatePasswordView(FormView):
-
-
-    template_name = 'users/update.html'
-    form_class = UpdatePasswordForm
-    success_url = reverse_lazy('users_app:user-login')
-    login_url = reverse_lazy('users_app:user-login')
-
-    def form_valid(self, form):
-
-        user_to_update = self.request.user
+@login_required(login_url='users_app:user-login')
+def update_password(request):
+    form = UpdatePasswordForm()
+    person = request.user
+    if request.method == 'POST':
+        form = UpdatePasswordForm(request.POST)
+        new_password = request.POST['password2']
         user = authenticate(
-            username=user_to_update.username,
-            password=form.cleaned_data['password1']
+            username=person.username,
+            password=request.POST['password1']
         )
-
         if user:
-            new_password = form.cleaned_data['password2']
-            user_to_update.set_password(new_password)
-            user_to_update.save()
-            user_to_update.is_active = True
+            if request.POST['password3'] != new_password:
+                messages.error(request, 'Niepoprawnie powtórzono nowe hasło.')
+            else:
+                user = request.user
+                user.set_password(new_password)
+                user.save()
+                logout(request)
+                return HttpResponseRedirect(
+                    reverse(
+                        'users_app:user-login'
+                    )
+                )
+        else:
+            messages.error(request, 'Niepoprawnie podano dotychczasowe hasło.')
+    return render(request, 'users/update_password.html', {'form': form})
 
-        logout(self.request)
-        messages.success(self.request, 'Twoje hasło zostało zmienione. Teraz możesz się zalogować')
-        return super(UpdatePasswordView, self).form_valid(form)
 
 class AllEmployeesList(TopManagerPermisoMixin, ListView):
 
@@ -132,31 +134,33 @@ class AllEmployeesList(TopManagerPermisoMixin, ListView):
     def get_context_data(self, **kwargs):
 
         context = super(AllEmployeesList, self).get_context_data(**kwargs)
-        all_employees = User.objects.all()
+        all_employees = User.objects.filter(is_active=True)
         today = date.today()
 
         for employee in all_employees:
             if employee.working_hours == 1.00:
-                employee.working_hours = 1 
+                employee.working_hours = 1
 
-            today_sick = Sickleave.objects.filter(Q(start_date__lte=today)&Q(end_date__gte= today)&Q(employee__id=employee.id)).all()
-            today_requests = Request.objects.filter(Q(start_date__lte = today)&Q(end_date__gte= today)&Q(author__id = employee.id)).exclude(status = "odrzucony").all()
+            today_sick = Sickleave.objects.filter(Q(start_date__lte=today) & Q(
+                end_date__gte=today) & Q(employee__id=employee.id)).all()
+            today_requests = Request.objects.filter(Q(start_date__lte=today) & Q(
+                end_date__gte=today) & Q(author__id=employee.id)).exclude(status="odrzucony").all()
 
             if len(today_sick) != 0:
                 employee.today_note = "C"
-            
+
             elif len(today_requests) != 0:
                 list_req = [tr.type for tr in today_requests]
                 employee.today_note = list_req[0]
-              
+
             else:
                 employee.today_note = "✓"
-              
+
         context['all_employees'] = all_employees
         return context
 
-class AdminEmployeesList(TopManagerPermisoMixin, ListView):
 
+class AdminEmployeesList(TopManagerPermisoMixin, ListView):
 
     template_name = "users/admin_all_employees.html"
     model = User
@@ -166,7 +170,8 @@ class AdminEmployeesList(TopManagerPermisoMixin, ListView):
     def get_context_data(self, **kwargs):
 
         context = super(AdminEmployeesList, self).get_context_data(**kwargs)
-        employees = User.objects.all()
+        employees = User.objects.order_by(
+            '-is_active', 'last_name', 'first_name').all()
 
         for employee in employees:
             if employee.working_hours == 1.00:
@@ -176,29 +181,30 @@ class AdminEmployeesList(TopManagerPermisoMixin, ListView):
             else:
                 employee.is_manager = "TAK"
 
-        
         context['employees'] = employees
         return context
 
-class EmpleadoUpdateView(TopManagerPermisoMixin, UpdateView):
 
+class EmployeeUpdateView(TopManagerPermisoMixin, UpdateView):
 
     model = User
     template_name = "users/update_employee.html"
     login_url = reverse_lazy('users_app:user-login')
-    
-    fields = ['username','email', 'work_email','first_name','last_name','position','position_addinfo',
-            'workplace','role','manager','working_hours','annual_leave','current_leave','contract_end',
-            'additional_info',]
+
+    fields = ['username', 'email', 'work_email', 'first_name', 'last_name', 'position', 'position_addinfo',
+              'workplace', 'role', 'manager', 'working_hours', 'annual_leave', 'current_leave', 'contract_end', 'is_active', 'is_superuser',
+              'additional_info', ]
 
     success_url = reverse_lazy('users_app:admin-all-employees')
 
+    def get_context_data(self, **kwargs):
+        context = super(EmployeeUpdateView, self).get_context_data(**kwargs)
+        context['form'].fields['manager'].queryset = User.objects.filter(
+            ~Q(role="P") & Q(is_active=True)).order_by('last_name')
+        return context
 
-def delete_employee(request,pk):
 
+@login_required(login_url='users_app:user-login')
+def delete_employee(request, pk):
     employee_to_delete = User.objects.get(id=pk).delete()
     return HttpResponseRedirect(reverse('users_app:admin-all-employees'))
-
-
-
-
