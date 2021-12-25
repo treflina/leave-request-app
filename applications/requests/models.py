51749886e6bca243a9f87346b2d96cde4baa265c
treflina <treflina@wp.pl>
2021-12-25 12:13:09 +0100

    is_active, on_delete settings when deleting user

--- applications/requests/models.py
+++ applications/requests/models.py
@@ -10,34 +10,36 @@ from .managers import RequestManager
 
 
 class Request(TimeStampedModel):
-    
+
     TYPE_CHOICES = (
         ('W', 'Urlop wypoczynkowy (W)'),
         ('WS', 'Wolne za pracującą sobotę (WS)'),
         ('WN', 'Wolne za pracę w niedzielę/święto (WN)'),
         ('DW', 'Wolne za święto przypadające w wolną sobotę (DW)'),
     )
-    
+
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE, related_name='request_user', default=''
     )
-    
-    type = models.CharField('Rodzaj',  max_length=30, choices=TYPE_CHOICES, default='')
-    work_date = models.DateField('Data pracującej sob./nd/św.', null=True, blank=True)
+
+    type = models.CharField('Rodzaj',  max_length=30,
+                            choices=TYPE_CHOICES, default='')
+    work_date = models.DateField(
+        'Data pracującej sob./nd/św.', null=True, blank=True)
     start_date = models.DateField('Od', null=True, blank=True)
     end_date = models.DateField('Do', null=True, blank=True)
-    days = models.PositiveIntegerField('Ilość dni urlopu', null=True, blank=True)
-    status = models.CharField(max_length=20, default ="oczekujący")
+    days = models.PositiveIntegerField(
+        'Ilość dni urlopu', null=True, blank=True)
+    status = models.CharField(max_length=20, default="oczekujący")
     substitute = models.CharField(max_length=50, blank=True)
-    send_to_person = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_manager', default="")
+    send_to_person = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null = True, related_name='user_manager', default="")
     signed_by = models.CharField(max_length=50, blank=True)
 
     objects = RequestManager()
 
     class Meta:
         verbose_name = 'Wnioski'
-        verbose_name_plural='Wnioski'
+        verbose_name_plural = 'Wnioski'
         ordering = ['-created']
-
- 
--- applications/users/models.py
+++ applications/users/models.py
@@ -10,7 +10,7 @@ from .managers import UserManager
 
 class User(AbstractBaseUser):
 
-    # check if AUTH_USER in base.py 
+    # check if AUTH_USER in base.py
 
     ROLE_CHOICES = (
         ('P', 'Nie'),
@@ -19,34 +19,39 @@ class User(AbstractBaseUser):
         ('S', 'Dyrektor')
     )
 
-    username = models.CharField('Nazwa użytkownika', max_length=15, unique=True)
+    username = models.CharField(
+        'Nazwa użytkownika', max_length=15, unique=True)
     email = models.EmailField('Email', null=True, blank=True)
     work_email = models.EmailField('Email służbowy', null=True, blank=True)
     first_name = models.CharField('Imię', max_length=30)
     last_name = models.CharField('Nazwisko', max_length=50)
     position = models.CharField('Stanowisko', max_length=50)
-    position_addinfo = models.CharField('Dopisek "sprz." w przypadku dwóch umów', max_length=50, blank =True)
-    workplace = models.CharField('Dział/Filia', max_length=50, blank = True)
-    role = models.CharField('Stanowisko kierownicze', null =True, max_length=10, choices=ROLE_CHOICES)
-    manager = models.ForeignKey('self', null=True, blank=True, related_name='user', on_delete=models.CASCADE)
-    working_hours = models.DecimalField('Wymiar etatu', max_digits=3, decimal_places=2, default=1)
+    position_addinfo = models.CharField(
+        'Dopisek "sprz." w przypadku dwóch umów', max_length=50, blank=True)
+    workplace = models.CharField('Dział/Filia', max_length=50, blank=True)
+    role = models.CharField('Stanowisko kierownicze',
+                            null=True, max_length=10, choices=ROLE_CHOICES)
+    manager = models.ForeignKey(
+        'self', null=True, blank=True, related_name='user', on_delete=models.SET_NULL)
+    working_hours = models.DecimalField(
+        'Wymiar etatu', max_digits=3, decimal_places=2, default=1)
     annual_leave = models.IntegerField('Roczny wymiar urlopu', default=26)
     current_leave = models.IntegerField('Urlop (pozostało)', default=0)
     contract_end = models.DateField('Umowa do:', null=True, blank=True)
-    is_staff = models.BooleanField('Admin', default=False)
-    is_active = models.BooleanField('Aktywny', default=True)
-    is_superuser = models.BooleanField(default=False)
-    additional_info = models.CharField('Dodatkowe informacje', blank=True, max_length=100)
-  
+    is_staff = models.BooleanField('is_staff', default=False)
+    is_active = models.BooleanField('Obecnie zatrudniony', default=True)
+    is_superuser = models.BooleanField(
+        'Uprawnienia administratora', default=False)
+    additional_info = models.CharField(
+        'Dodatkowe informacje', blank=True, max_length=100)
 
     USERNAME_FIELD = 'username'
 
-    
     objects = UserManager()
 
     class Meta:
         verbose_name = 'Pracownik'
-        verbose_name_plural='Pracownicy'
+        verbose_name_plural = 'Pracownicy'
         ordering = ['last_name', 'first_name']
 
     def has_perm(self, perm, obj=None):
@@ -58,6 +63,5 @@ class User(AbstractBaseUser):
     def get_full_name(self):
         return self.first_name+" "+self.last_name
 
-    
     def __str__(self):
-        return self.last_name + " "+ self.first_name
+        return self.last_name + " " + self.first_name
